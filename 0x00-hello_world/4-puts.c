a5ee120cf0079ef4dd84789e6adda168b4a21402
Abdulrahman <aliyuabdulrahman741@gmail.comm>
2023-02-16 16:08:05 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that print a line with put function 
+ * Return: 0 (success)
+ */
+Int main(void)
+{
+Puts("\"programing is like multilingual puzzle");
+return (0);
+}
